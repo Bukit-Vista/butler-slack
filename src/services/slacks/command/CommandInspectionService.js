67b174e81c3e99350a3578c18cb3a22d2a812ed6
RobinNusantara <robinnusantara127@gmail.com>
2023-01-18 14:39:46 +0700

    [CHG] inspection block

--- src/services/slacks/command/CommandInspectionService.js
+++ src/services/slacks/command/CommandInspectionService.js
@@ -1,6 +1,5 @@
 const { MakePropertyApiService } = require("@service/make/MakePropertyService");
 const { decorate, injectable, inject } = require("inversify");
-const { Blocks, Elements, Modal, Option } = require("slack-block-builder");
 const { SlackCommandService } = require("../base/SlackCommandService");
 
 class CommandInspectionService extends SlackCommandService {
@@ -30,46 +29,124 @@ class CommandInspectionService extends SlackCommandService {
 
         return {
             ["trigger_id"]: values.trigger.id,
-            view: Modal({
-                callbackId: "inspection-modal",
-                title: "Submit Inspection",
-                submit: "Submit",
-            })
-                .blocks(
-                    Blocks.Input({
-                        blockId: "title_block",
-                        label: "Title",
-                    }).element(
-                        Elements.TextInput({
-                            actionId: "title",
-                        }),
-                    ),
-                    Blocks.Input({
-                        blockId: "description_block",
-                        label: "Description",
-                        optional: true,
-                    }).element(
-                        Elements.TextInput({
-                            actionId: "description",
-                        }),
-                    ),
-                    Blocks.Input({
-                        blockId: "properties_block",
-                        label: "Property",
-                    }).element(
-                        Elements.StaticSelect({
-                            actionId: "properties",
-                        }).options(
-                            properties.map((property) =>
-                                Option({
-                                    text: property["property"],
-                                    value: property["rowId"],
-                                }),
-                            ),
-                        ),
-                    ),
-                )
-                .buildToJSON(),
+            view: {
+                title: {
+                    type: "plain_text",
+                    text: "QC Inspection",
+                    emoji: true,
+                },
+                submit: {
+                    type: "plain_text",
+                    text: "Submit",
+                    emoji: true,
+                },
+                type: "modal",
+                close: {
+                    type: "plain_text",
+                    text: "Cancel",
+                    emoji: true,
+                },
+                blocks: [
+                    {
+                        type: "section",
+                        text: {
+                            type: "plain_text",
+                            text: ":wave: Hey [Name]!",
+                            emoji: true,
+                        },
+                    },
+                    {
+                        type: "section",
+                        text: {
+                            type: "mrkdwn",
+                            text: "Please select property",
+                        },
+                        accessory: {
+                            type: "static_select",
+                            placeholder: {
+                                type: "plain_text",
+                                text: "Select property",
+                                emoji: true,
+                            },
+                            options: properties.map((property) => ({
+                                text: {
+                                    type: "plain_text",
+                                    text: `*${property["property"]}*`,
+                                    emoji: true,
+                                },
+                                value: property["rowId"],
+                            })),
+                            action_id: "static_select-action",
+                        },
+                    },
+                    {
+                        type: "divider",
+                    },
+                    {
+                        type: "section",
+                        text: {
+                            type: "mrkdwn",
+                            text: "Set property status",
+                        },
+                        accessory: {
+                            type: "static_select",
+                            placeholder: {
+                                type: "plain_text",
+                                text: "Select status",
+                                emoji: true,
+                            },
+                            options: [
+                                {
+                                    text: {
+                                        type: "plain_text",
+                                        text: "*High risk*",
+                                        emoji: true,
+                                    },
+                                    value: "High risk",
+                                },
+                                {
+                                    text: {
+                                        type: "plain_text",
+                                        text: "*Medium risk*",
+                                        emoji: true,
+                                    },
+                                    value: "Medium risk",
+                                },
+                                {
+                                    text: {
+                                        type: "plain_text",
+                                        text: "*Low risk*",
+                                        emoji: true,
+                                    },
+                                    value: "Low risk",
+                                },
+                                {
+                                    text: {
+                                        type: "plain_text",
+                                        text: "*No risk*",
+                                        emoji: true,
+                                    },
+                                    value: "No risk",
+                                },
+                            ],
+                            action_id: "static_select-action",
+                        },
+                    },
+                    {
+                        type: "input",
+                        label: {
+                            type: "plain_text",
+                            text: "Report description",
+                            emoji: true,
+                        },
+                        element: {
+                            type: "plain_text_input",
+                            multiline: true,
+                        },
+                        optional: false,
+                    },
+                ],
+            },
         };
     }
 }
