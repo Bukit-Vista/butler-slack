d29d7a1d3ac7e8a54e1b8ffa8e8e6a7a8a728e01
Bayu Stiawan <bayu@stiawan.com>
2023-02-12 21:51:10 +0800

    Update app.js

--- app.js
+++ app.js
@@ -38,7 +38,7 @@ app.error((error) => {
 
 // Listens to incoming webhooks
 receiver.router.get('/', (req, res) => { res.end('⚡ Bolt app is running!'); });
-receiver.router.post('/events', (req, res) => { res.end('Ok'); webhook.notesWebhook(req, app) });
+receiver.router.post('/notes', (req, res) => { res.end('Ok'); webhook.notesWebhook(req, app) });
 
 (async () => {
   // Start your app
