83e87780904543d1fbcf6d0e76cc55ed6b1aee35
Bayu Stiawan <bayu@stiawan.com>
2023-02-12 22:20:47 +0800

    Event switch

--- controller/webhook/index.js
+++ controller/webhook/index.js
@@ -1,7 +1,22 @@
 const { notesWebhook } = require('./notesWebhook');
 
-const webhook = {
-    notesWebhook
-}
 
-module.exports = webhook;
+module.exports = {
+    events: (req, res, app) => {
+
+        const data = req.body;
+
+        // Switch on the action type
+        switch (data.action) {
+            case 'notes.insert':
+                res.end('Event received');
+                notesWebhook(req, app);
+                break;
+            default:
+                console.log('Event handler not found', data.action);
+                res.writeHead(404);
+                res.end('Event handler not found');
+        }
+
+    }
+}
